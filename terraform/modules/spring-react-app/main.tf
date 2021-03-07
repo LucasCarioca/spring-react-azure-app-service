5ce358baf29f85388424e824f7fe458b675823fc
Lucas Desouza <lucasfds2777@gmail.com>
2021-03-07 13:41:35 -0600

    adding storage account resource

--- terraform/modules/spring-react-app/main.tf
+++ terraform/modules/spring-react-app/main.tf
@@ -28,3 +28,17 @@ resource "azurerm_app_service" "appservice" {
 
   app_settings = {}
 }
+
+resource "azurerm_storage_account" "staticwebapp" {
+  name                      = "${var.name}webapp${var.env}"
+  resource_group_name       = azurerm_resource_group.rg.name
+  location                  = azurerm_resource_group.rg.location
+  account_tier              = "Standard"
+  account_replication_type  = "LRS"
+  enable_https_traffic_only = true
+
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "index.html"
+  }
+}
