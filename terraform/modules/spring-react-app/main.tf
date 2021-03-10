38b45b57a3b3c0d4c12d1ce39dcd7641ef102435
Lucas Desouza <lucasfds2777@gmail.com>
2021-03-09 21:36:32 -0600

    remove linux from app service

--- terraform/modules/spring-react-app/main.tf
+++ terraform/modules/spring-react-app/main.tf
@@ -7,8 +7,6 @@ resource "azurerm_app_service_plan" "serviceplan" {
   name                = "${var.env}-appserviceplan"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  kind                = "Linux"
-  reserved            = true
 
   sku {
     tier = "Basic"
