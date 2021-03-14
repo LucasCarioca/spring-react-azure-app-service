87dc594c5ab52c5a8b364bfc16f882d507f8c04e
Lucas Desouza <lucasfds2777@gmail.com>
2021-03-13 19:41:15 -0600

    fix tf and cleanup ignore files

--- terraform/modules/spring-react-app/main.tf
+++ terraform/modules/spring-react-app/main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_app_service_plan" "serviceplan" {
-  name                = "${var.be_name}-appserviceplan-${var.env}}"
+  name                = var.be_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
@@ -15,7 +15,7 @@ resource "azurerm_app_service_plan" "serviceplan" {
 }
 
 resource "azurerm_app_service" "appservice" {
-  name                = "${var.be_name}-${var.env}"
+  name                = var.be_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.serviceplan.id
